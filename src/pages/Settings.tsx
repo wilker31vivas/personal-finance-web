95cd351715bcffd3c6a17c471d903c18afae1d88
Wilker Alexander Vivas Colmenares <92475759+wilker31vivas@users.noreply.github.com>
2026-02-27 12:07:06 -0400

    Remove unused useEffect import in Settings.tsx

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ModalProfile } from "../components/Modal";
 import { useSettings } from "../context/SettingsContext";
 
