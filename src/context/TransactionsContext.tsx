97c482e317f7065c47e47e20c7c91d56d44a1c04
wilker <wilker.vivas.31@gmail.com>
2026-03-07 01:09:23 -0400

    add: custom hook useTransactionsFetch

--- src/context/TransactionsContext.tsx
+++ src/context/TransactionsContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, useState, useEffect, useContext } from "react";
-import { getTransactions, getCategories } from '../api/transactions'
+import { createContext, useContext } from "react";
 import type { Transaction, Filters, UpdateFilterType, Category } from "../types/types"
+import useTransactionsFetch from "../hooks/useTransactionsFetch";
 
 type TransactionsContextType = {
     transactions: Transaction[]
@@ -25,88 +25,19 @@ type TransactionsContextType = {
 
 export const TransactionsContext = createContext<TransactionsContextType | null>(null)
 
-export const INITIAL_FILTERS: Filters = { month: "", year: "", type: "", category: "" }
-
-const INITIAL_TRANSACTION: Transaction = {
-    id: "",
-    description: '',
-    amount: 0,
-    type: 'expense',
-    category: '',
-    date: ''
-}
-
-export const RESULT_PER_PAGE = 5
+// useTransactions()     fetch + filters
+// usePagination()       currentPage, totalPages
+// useTransactionForm()  formData, modales
 
 export function TransactionsContextProvider({ children }: { children: React.ReactNode }) {
-    const [transactions, setTransactions] = useState<Transaction[]>([])
-    const [currentPage, setCurrentPage] = useState(1)
-
-    const totalPages = Math.ceil(transactions.length / RESULT_PER_PAGE)
-    const pagedResults = transactions.slice(
-        (currentPage - 1) * RESULT_PER_PAGE,
-        currentPage * RESULT_PER_PAGE
-    )
-
-    const [error, setError] = useState<string | null>(null)
-    const [loading, setLoading] = useState(false)
-    const [filters, setFilters] = useState<Filters>(INITIAL_FILTERS)
-    const [categories, setCategories] = useState<Category[] | null>(null)
-    const [isModalEditOpen, setIsModalEditOpen] = useState(false);
-    const [isModalDeleteOpen, setIsModalDeleteOpen] = useState(false);
-    const [formData, setFormData] = useState<Transaction>(INITIAL_TRANSACTION);
-
-    const resetFilters = () => {
-        setFilters(INITIAL_FILTERS)
-    }
-
-    const updateFilter = <K extends keyof Filters>(key: K, value: Filters[K]) => {
-        setFilters(prev => ({ ...prev, [key]: value || "" }))
-        setCurrentPage(1)
-    }
-
-    const onPageChange = (page: number) => {
-        setCurrentPage(page)
-    }
-
-    async function loadData() {
-        setLoading(true)
-        setError(null)
-        try {
-            const t = await getTransactions(filters)
-            setTransactions(t);
-
-        } catch (err) {
-            setError(err instanceof Error ? err.message : 'Error loading transactions')
-        }
-        finally {
-            setLoading(false)
-        }
-    }
-
-    useEffect(() => {
-        loadData()
-    }, [filters])
-
-    useEffect(() => {
-        const fetchCategories = async () => {
-            try {
-                const res = await getCategories()
-                setCategories(res)
-            } catch (err) {
-                setError(`Error loading categories: ${err}`)
-            }
-        }
-
-        fetchCategories()
-    }, [])
+    const { transactions, loading, error, filters, updateFilter, resetFilters, categories, loadData } = useTransactionsFetch()
 
     return (
         <TransactionsContext.Provider value={{
             transactions, error, loading,
             filters, updateFilter, resetFilters,
-            currentPage, onPageChange,
-            categories, loadData, totalPages,
+            loadData, categories, currentPage, 
+            onPageChange, totalPages,
             pagedResults, formData, setFormData,
             isModalDeleteOpen, setIsModalDeleteOpen,
             isModalEditOpen, setIsModalEditOpen
---
+++ src/hooks/useTransactionsFetch.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect, useCallback } from "react";
+import { getTransactions, getCategories } from '../api/transactions'
+import type { Transaction, Filters, Category } from "../types/types"
+
+export const INITIAL_FILTERS: Filters = { month: "", year: "", type: "", category: "" }
+
+export default function useTransactions() {
+    const [loading, setLoading] = useState(false)
+    const [filters, setFilters] = useState<Filters>(INITIAL_FILTERS)
+    const [error, setError] = useState<string | null>(null)
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+    const [categories, setCategories] = useState<Category[] | null>(null)
+
+    const loadData = async () => {
+        setLoading(true)
+        setError(null)
+        try {
+            const t = await getTransactions(filters)
+            setTransactions(t);
+
+        } catch (err) {
+            setError(err instanceof Error ? err.message : 'Error loading transactions')
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
+    const resetFilters = useCallback(() => {
+        setFilters(INITIAL_FILTERS)
+    }, [])
+
+    const updateFilter = useCallback(<K extends keyof Filters>(key: K, value: Filters[K]) => {
+        setFilters(prev => ({ ...prev, [key]: value || "" }))
+    }, [])
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const res = await getCategories()
+                setCategories(res)
+            } catch (err) {
+                setError(err instanceof Error ? err.message : 'Error loading categories')
+            }
+        }
+
+        fetchCategories()
+    }, [])
+
+    useEffect(() => {
+        loadData()
+    }, [filters])
+
+    return {
+        loading, error, transactions, loadData, resetFilters, updateFilter, categories, filters
+    }
+}
