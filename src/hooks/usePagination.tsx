5806e582815cdb9482c62bff200e647fe89555c8
wilker <wilker.vivas.31@gmail.com>
2026-03-07 01:10:33 -0400

    add: custom hook usePagination

---
+++ src/hooks/usePagination.tsx
@@ -0,0 +1,28 @@
+import { useState, useMemo, useCallback } from "react";
+import type { Transaction } from "../types/types"
+
+export const RESULT_PER_PAGE = 5
+
+export default function usePagination(transactions: Transaction[]) {
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const totalPages = useMemo(
+        () => Math.ceil(transactions.length / RESULT_PER_PAGE),
+        [transactions]
+    )
+    const pagedResults = useMemo(
+        () => transactions.slice(
+            (currentPage - 1) * RESULT_PER_PAGE,
+            currentPage * RESULT_PER_PAGE
+        ),
+        [transactions, currentPage]
+    )
+
+    const onPageChange = useCallback((page: number) => {
+        setCurrentPage(page)
+    }, [])
+
+    return {
+        totalPages, pagedResults, onPageChange, currentPage
+    }
+}
