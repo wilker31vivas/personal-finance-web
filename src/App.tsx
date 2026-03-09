7ac41da7f3cc0722fd3e42d128bc36db02188311
wilker <wilker.vivas.31@gmail.com>
2026-03-09 15:44:39 -0400

    add lazy loading on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import Dashboard from "./pages/Dashboard"
+import { Route, Routes } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
 import { DashboardContextProvider } from "./context/DashboardContext"
 import { TransactionsContextProvider } from "./context/TransactionsContext"
-import Transactions from "./pages/Transactions"
-import Categories from "./pages/Categories"
-import Aside from "./components/Aside"
-import { Route, Routes } from 'react-router-dom'
-import NotFound from "./components/NotFound"
-import Settings from "./pages/Settings"
-import Login from "./components/Login"
 import { useSettings } from "./context/SettingsContext"
+import Aside from "./components/Aside"
+import Login from "./components/Login"
+import {LoadingFallback} from './components/Fallback'
+
+const Dashboard = lazy(() => import("./pages/Dashboard"))
+const Transactions = lazy(() => import("./pages/Transactions"))
+const Categories = lazy(() => import("./pages/Categories"))
+const Settings = lazy(() => import("./pages/Settings"))
+const NotFound = lazy(() => import("./components/NotFound"))
 
 function App() {
   const { user } = useSettings()
@@ -19,23 +22,25 @@ function App() {
     <div className='h-screen flex overflow-hidden bg-background-light dark:bg-background-dark text-slate-900 '>
       <Aside />
       <main className=" flex-1 overflow-y-auto transition-colors duration-200">
-        <Routes>
-          <Route path="/" element={
-            <DashboardContextProvider>
-              <Dashboard />
-            </DashboardContextProvider>}>
-          </Route>
-          <Route path="/categories" element={<Categories />}/>
-          <Route path="/transactions" element={
-            <TransactionsContextProvider>
-              <Transactions />
-            </TransactionsContextProvider>}>
-          </Route>
-          <Route path="/settings" element={
-            <Settings></Settings>
-          }/>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<LoadingFallback />}>
+          <Routes>
+            <Route path="/" element={
+              <DashboardContextProvider>
+                <Dashboard />
+              </DashboardContextProvider>}>
+            </Route>
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/transactions" element={
+              <TransactionsContextProvider>
+                <Transactions />
+              </TransactionsContextProvider>}>
+            </Route>
+            <Route path="/settings" element={
+              <Settings></Settings>
+            } />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </main>
     </div>
   )
